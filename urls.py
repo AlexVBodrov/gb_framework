911f14de69555cbf5f47b94a1ec41632904748c4
AlexVBodrov <alexvbodrov@mail.ru>
2023-02-09 01:38:25 +0300

    create patterns

--- urls.py
+++ urls.py
@@ -3,6 +3,15 @@ from views import *
 
 
 # front controller
+def secret_front(request):
+    request['date'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
 
 # PC
 routes = {
@@ -11,7 +20,10 @@ routes = {
     '/#/': Index(),
     '/about/': About(),
     '/contact/': Contact(),
-    '/page/': Page(),
-    '/examples/': Examples(),
-    '/another_page/': AnotherPage(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
 }
